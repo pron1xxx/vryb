757f38dea9d271b15086d1d4ebb3c245189d6eb0
pron1xxx <129777496+pron1xxx@users.noreply.github.com>
2026-07-07 11:44:58 +0300

    Add files via upload

---
+++ config/const.php
@@ -0,0 +1,15 @@
+<?php 
+
+define('ROOT', dirname(__DIR__));
+define('PATH', 'https://vryb.local/');
+define('CORE', ROOT . '/vendor/mycls/core');
+define('CONFIG', ROOT . '/config');
+define('APP', ROOT . '/app');
+define('CONTROLLERS', APP . '/controllers');
+define('VIEWS', APP . '/views');
+define('PUBLICF', ROOT . '/public');
+define('VENDOR', ROOT . '/vendor');
+
+define('ASSETS', '/public/assets');
+define('CSS', ASSETS . '/css');
+define('IMAGES', ASSETS . '/images');
---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php 
+
+return  [
+    'dbhost' => 'localhost',
+    'dbname' => 'cu512526_vrybdata',
+    'username' => 'cu512526_vrybdata',
+    'password' => 'ieBa2pNsyU!',
+    'charset' => 'utf8mb4',
+    'persistent' => false,  
+    'options' => [
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        // Дополнительные опции по умолчанию уже добавлены в классе Db
+    ]
+];
